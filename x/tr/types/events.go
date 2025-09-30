398edaacccbc7d95ed963c6f5ca73c66a627d189
pratikasr <pratikasr@gmail.com>
2025-10-01 00:21:26 +0530

    feat: added events for msgs in tr module
    Signed-off-by: pratikasr <pratikasr@gmail.com>

--- x/tr/types/events.go
+++ x/tr/types/events.go
@@ -4,6 +4,10 @@ const (
 	EventTypeCreateTrustRegistry               = "create_trust_registry"
 	EventTypeCreateGovernanceFrameworkVersion  = "create_governance_framework_version"
 	EventTypeCreateGovernanceFrameworkDocument = "create_governance_framework_document"
+	EventTypeAddGovernanceFrameworkDocument    = "add_governance_framework_document"
+	EventTypeIncreaseActiveGFVersion           = "increase_active_gf_version"
+	EventTypeUpdateTrustRegistry               = "update_trust_registry"
+	EventTypeArchiveTrustRegistry              = "archive_trust_registry"
 
 	AttributeKeyTrustRegistryID = "trust_registry_id"
 	AttributeKeyDID             = "did"
@@ -17,4 +21,5 @@ const (
 	AttributeKeyDocURL          = "doc_url"
 	AttributeKeyDigestSri       = "digest_sri"
 	AttributeKeyDeposit         = "deposit"
+	AttributeKeyArchiveStatus   = "archive_status"
 )
