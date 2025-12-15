79bc8c7fd9c62d52beaa61fa1393bf6c743af5d8
pratikasr <pratikasr@gmail.com>
2025-12-15 18:47:58 +0530

    feat: added v0.9.2 upgrade code.
    Signed-off-by: pratikasr <pratikasr@gmail.com>

--- app/upgrades/upgrades.go
+++ app/upgrades/upgrades.go
@@ -3,10 +3,12 @@ package upgrades
 import (
 	"github.com/verana-labs/verana/app/upgrades/types"
 	v091 "github.com/verana-labs/verana/app/upgrades/v091"
+	v092 "github.com/verana-labs/verana/app/upgrades/v092"
 	v9 "github.com/verana-labs/verana/app/upgrades/v9"
 )
 
 var Upgrades = []types.Upgrade{
+	v092.Upgrade,
 	v9.Upgrade,
 	v091.Upgrade,
 }
