905a662c14237fd448c76b96b692832f5485c98f
Mathieu Gauthron <mathieu@matlux.net>
2026-02-04 17:19:09 +0000

    Expose amino converters in ts-proto package

--- ts-proto/src/index.ts
+++ ts-proto/src/index.ts
@@ -1,6 +1,4 @@
 /**
- * Entry point placeholder.
- * Consumers typically import directly from codec paths, e.g.:
- *   import { MsgAddDID } from "@verana/proto-codecs/codec/verana/dd/v1/tx";
+ * Entry point for the Verana TypeScript codecs.
  */
-export {};
+export * from "./helpers/aminoConverters";
